3938dd48710133db454a49a486857e16d5caad11
CyrilOkidi <okidicyril@gmail.com>
2022-04-06 17:20:06 +0300

    Update product result row component

--- src/App.js
+++ src/App.js
@@ -30,12 +30,61 @@ const ResultLegend = () => {
   );
 };
 
+const ProductCategoryContainer = () => {
+  return (
+    <div className="product-category-container">
+      <div className="product-category-item">
+        <p>問題1a</p>
+        <div className="product-category-answer-container">
+          <b className="product-category-answer active-answer">A</b>
+          <b className="product-category-answer default-answer">B</b>
+          <b className="product-category-answer inactive-answer">C</b>
+          <b className="product-category-answer active-answer">D</b>
+          <b className="product-category-answer active-answer">E</b>
+        </div>
+      </div>
+
+      <div className="product-category-item">
+        <p>問題1b</p>
+        <div className="product-category-answer-container">
+          <b className="product-category-answer active-answer">A</b>
+          <b className="product-category-answer default-answer">B</b>
+          <b className="product-category-answer inactive-answer">C</b>
+        </div>
+      </div>
+
+      <div className="product-category-item">
+        <p>問題1c</p>
+        <div className="product-category-answer-container">
+          <b className="product-category-answer active-answer">A</b>
+          <b className="product-category-answer default-answer">B</b>
+          <b className="product-category-answer inactive-answer">C</b>
+          <b className="product-category-answer active-answer">D</b>
+          <b className="product-category-answer active-answer">E</b>
+        </div>
+      </div>
+
+      <div className="product-category-item">
+        <p>問題2</p>
+        <p>最長至100歲</p>
+      </div>
+
+      <div className="product-category-item">
+        <p>簡易分析</p>
+        <p>問題2</p>
+      </div>
+    </div>
+  );
+};
+
 const ProductResultContainer = () => {
   return (
     <div className="product-result-container">
       <div className="product-name">
         <p>豐進儲蓄計劃</p>
       </div>
+
+      <ProductCategoryContainer />
     </div>
   );
 };
